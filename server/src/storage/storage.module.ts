e7323440db36fc47605adcab679c0f3173bfab16
Uladzislau Kviatkouski <95105135+wladyslawkrz@users.noreply.github.com>
2024-07-28 13:12:53 +0300

    create client nextjs app

--- server/src/storage/storage.module.ts
+++ server/src/storage/storage.module.ts
@@ -2,11 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { MinioClientFactory } from "./minio.factory";
 import { StorageService } from "./storage.service";
-import { StorageController } from "./storage.controller";
 
 @Module({
   providers: [MinioClientFactory, StorageService],
-  controllers: [StorageController],
   exports: [StorageService],
 })
 export class StorageModule {}
